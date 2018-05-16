d5e72f76c105cb1e7771b13d8b78ceb9da7f0f08
vitaliy.filaretov <v_filaretov@bk.ru>
2018-05-17 01:23:53 +0300

    зачистка

--- info.py
+++
@@ -1,4 +0,0 @@
-user_info = {'first_name': 'Виталий', 'last_name': 'Прот'}
-last = input ('Введите фамилию ')
-user_info ['last_name'] = last
-print (user_info)
--- test.py
+++
@@ -1,9 +0,0 @@
-# slovar = {'Виталий':{'city':'Moscow', 'temperature': 20, 'age': 12}}
-# que = (input('Введите имя '))
-# #print(que)
-# print(slovar.get(que, 'Ничего нет'))
-
-
-a= 2
-b= 3
-print( a+ b)
